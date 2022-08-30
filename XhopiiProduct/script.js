c79d65ecdfc062cab4d18d5c7f04c010f9f3a70c
karol <112397555+karouliz@users.noreply.github.com>
2022-08-30 12:27:34 -0300

    Add files via upload

---
+++ XhopiiProduct/script.js
@@ -0,0 +1,21 @@
+function selecionarImgProduto(imgMiniaturaEscolhida){
+    document.getElementById("imgPrincipal").src = imgMiniaturaEscolhida.src;
+}
+
+function mudarPreco(botaoEscolhido){
+    if(botaoEscolhido.id == "botaoAmarela"){
+        document.getElementById('precoProduto').innerHTML = "R$376,88";
+    }
+    else if(botaoEscolhido.id == "botaoAzul"){
+        document.getElementById('precoProduto').innerHTML = "R$219,90";
+    }
+    else if(botaoEscolhido.id == "botaoNomeNumero"){
+        document.getElementById('precoProduto').innerHTML = "R$499,99";
+    }
+}
+
+function mudarTamanho(tamanhoEscolhido){
+     if(tamanhoEscolhido.id == "p"){
+         document.getElementById('pecasDisponiveis')
+     }
+}
